50311c7ae5e723ac0bfdae12ca0346ba78ce6a41
Jakub Chodorowski <chodorowski.jakub@gmail.com>
2018-10-12 13:41:26 +0200

    stworzenie 2 pustych acitivity odnoszacych sie do dodawania boiska

---
+++ app/src/main/java/pl/smartplayer/smartplayerapp/field/ChooseFieldActivity.java
@@ -0,0 +1,15 @@
+package pl.smartplayer.smartplayerapp.field;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.smartplayer.smartplayerapp.R;
+
+public class ChooseFieldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_field);
+    }
+}
