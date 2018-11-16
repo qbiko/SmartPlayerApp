5900c3d06fe4b47e8be0398c5e9c8e068c9291e3
Jakub Chodorowski <chodorowski.jakub@gmail.com>
2018-11-16 12:14:30 +0100

    poczatek implementacji aktywnosci dodawania zawodnikow

---
+++ app/src/main/java/pl/smartplayer/smartplayerapp/player/PlayerListActivity.java
@@ -0,0 +1,15 @@
+package pl.smartplayer.smartplayerapp.player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.smartplayer.smartplayerapp.R;
+
+public class PlayerListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_list);
+    }
+}
